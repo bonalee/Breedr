43105b5bfb42f5ce005e5882a42fdadb0bf77841
Bona Lee <bonalee3@gmail.com>
2017-09-07 19:24:17 -0500

    Confirmed matches shows for incoming and outgoing

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -7,7 +7,6 @@ class MatchesController < ApplicationController
 
     @outgoing_matches = []
     @incoming_matches = []
-
     current_user.pets.each do |current_user_pet|
       pet_outmatch = Match.find_by(pet_id: current_user_pet.id)
       @pet_info = Pet.find_by(id: params[:pet_id])
@@ -15,13 +14,30 @@ class MatchesController < ApplicationController
         @outgoing_matches << pet_outmatch
       end
     end
-
     current_user.pets.each do |current_user_pet|
       pet_inmatch = Match.find_by(inverse_pet_id: current_user_pet.id)
       if pet_inmatch != nil
         @incoming_matches << pet_inmatch
       end
     end
+
+    @confirmed_matches = []
+    @outgoing_matches.each do |outgoing_match|
+      if outgoing_match.matched_status == "Confirmed"
+        @confirmed_matches << outgoing_match
+      end
+    end
+    @incoming_matches.each do |incoming_match|
+      if incoming_match.matched_status == "Confirmed"
+        @confirmed_matches << incoming_match
+      end
+    end
+
+    @current_user_petids = []
+    current_user.pets.each do |current_user_pet|
+      @current_user_petids << current_user_pet.id
+    end
+
     render "index.html.erb"
   end
 
